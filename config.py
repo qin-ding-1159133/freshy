26db6a03bff423562a77ef285c592efee57d8af8
qin-ding-1159133 <qin.ding@lincolnuni.ac.nz>
2024-07-12 19:25:18 +1200

    up

--- config.py
+++
@@ -1,3 +0,0 @@
-mail_username = "postmaster@sandbox847cda5fa43e4883817d48eb95ffc3da.mailgun.org"
-mail_password = "d3f38312be1b16a9699813539c1182bc-2fbe671d-45fa3849"
-mail_sender = "hifreshharvestdelivery@outlook.com"
